9ba96620910c540c0e00be6d0a2f7bc017734b3b
Mayar0303 <112547252+Mayar0303@users.noreply.github.com>
2023-06-06 22:14:29 +0200

    Add indexController

---
+++ Models/DishViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace YumMe.Models;
+
+ public class DishViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Cuisine { get; set; }
+    public Uri FoodImage { get; set; }
+}
