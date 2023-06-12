f67c931b0ca0af00412cbaed8c75ce3bad7c797c
Dovydas Norvilas <david59965@hotmail.com>
2023-06-12 15:16:04 +0200

    Fixed click counter

--- Data/DataSeed.sql
+++ Data/DataSeed.sql
@@ -1,22 +1,3 @@
--- Dishes
-SET IDENTITY_INSERT YumMe.dbo.Dishes ON;
-INSERT INTO Dishes (ID, Name, FoodImage, Spicy, CuisineId)
-VALUES
-    (1, 'Steak', 'images/BiffTimjan.jpg', 'Medium', (SELECT id from Cuisines WHERE Id='1' )),
-    (2, 'Dragon Roll', 'images/Dragonroll.jpg', 'Mild', (SELECT id from Cuisines WHERE Id='2' )),
-    (3, 'Hamburger', 'images/hamburgare.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='3' )),
-    (4, 'Butter Chicken', 'images/indisk.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='4' )),
-    (5, 'Chicken Basili', 'images/Khaphrokai.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='5' )),
-    (6, 'Lamb BBQ', 'images/Lamm.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='1' )),
-    (7, 'Sushi Mix', 'images/Mix.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='2' )),
-    (8, 'Phad Thai', 'images/PhadThai.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='5' )),
-    (9, 'Pokebowl', 'images/Pokebowl.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='2' )),
-    (10, 'Shrimp With Rice', 'images/räkormedris.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='5' )),
-    (11, 'Salmon Aspagrus', 'images/Salmon.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='1' )),
-    (12, 'Sashimi', 'images/Sashimi.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='2' )),
-    (13, 'Sea Food Pasta', 'images/SeafoodPasta.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='14' )),
-    (14, 'Shrimp Pasta', 'images/shrimpPasta.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='14' ));
-SET IDENTITY_INSERT YumMe.dbo.Dishes OFF;
 
 -- Cuisines
 SET IDENTITY_INSERT YumMe.dbo.Cuisines ON;
@@ -32,6 +13,25 @@ VALUES
 SET IDENTITY_INSERT YumMe.dbo.Cuisines OFF;
 
 
+-- Dishes
+SET IDENTITY_INSERT YumMe.dbo.Dishes ON;
+INSERT INTO Dishes (ID, Name, FoodImage, Spicy, CuisineId)
+VALUES
+    (1, 'Steak', 'images/BiffTimjan.jpg', 'Medium', (SELECT id from Cuisines WHERE Id='1' )),
+    (2, 'Dragon Roll', 'images/Dragonroll.jpg', 'Mild', (SELECT id from Cuisines WHERE Id='2' )),
+    (3, 'Hamburger', 'images/hamburgare.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='3' )),
+    (4, 'Butter Chicken', 'images/indisk.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='4' )),
+    (5, 'Chicken Basili', 'images/Khaphrokai.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='5' )),
+    (6, 'Lamb BBQ', 'images/Lamm.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='1' )),
+    (7, 'Sushi Mix', 'images/Mix.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='2' )),
+    (8, 'Phad Thai', 'images/PhadThai.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='5' )),
+    (9, 'Pokebowl', 'images/Pokebowl.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='2' )),
+    (10, 'Shrimp With Rice', 'images/räkormedris.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='5' )),
+    (11, 'Salmon Asparagus', 'images/Salmon.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='1' )),
+    (12, 'Sashimi', 'images/Sashimi.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='2' )),
+    (13, 'Sea Food Pasta', 'images/SeafoodPasta.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='14' )),
+    (14, 'Shrimp Pasta', 'images/shrimpPasta.jpg', 'Non-Spicy', (SELECT id from Cuisines WHERE Id='14' ));
+SET IDENTITY_INSERT YumMe.dbo.Dishes OFF;
 
 
 -- Restaurants
